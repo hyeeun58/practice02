907b0d2931dc635122f74d456b80ab0834fe75a3
BIT-USER <BIT-USER@DESKTOP-CVU2JQ2>
2017-08-01 17:50:27 +0900

    test

--- src/prob1/Prob1.java
+++ src/prob1/Prob1.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Prob1 {
 	public static void main(String[] args) 
 	{
+		
+		
+		
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
 		
 		Scanner s = new Scanner(System.in);
